918572098e4ebd90cb073807101eedffc7adb291
Michael Tombor <tombormt@gmail.com>
2018-04-22 05:30:45 +0000

    add .env file to gitignore and added mlab URI

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds153869.mlab.com:53869/auth`;
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
