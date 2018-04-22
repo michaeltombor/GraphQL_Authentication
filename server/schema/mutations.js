87395a0f3daddab48e9952c7ca9fa7fd3616f21b
Michael Tombor <tombormt@gmail.com>
2018-04-22 06:13:27 +0000

    add sign in mutation and imported AuthService.signup to handle sign up logic

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -4,11 +4,21 @@ const {
     GraphQLString
 } = graphql;
 const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
 
 const mutation = new GraphQLObjectType ({
     name: 'Mutation',
     fields: {
-        
+        signup: {
+            type: UserType,
+            args: {
+                email: { type: GraphQLString },
+                password: { type: GraphQLString }
+            },
+            resolve(parentValue, { email, password }, req) {
+                return AuthService.signup({ email, password, req })
+            }
+        }
     }
 })
 
