ed23268c267451e0d965f16c84bed7effa2d6648
Michael Tombor <tombormt@gmail.com>
2018-04-27 06:33:17 +0000

    add email and password to AuthForm

--- client/index.js
+++ client/index.js
@@ -5,7 +5,8 @@ import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-
+import LoginForm from './components/LoginForm';
+ 
 const networkInterface = createNetworkInterface({
   uri: '/graphql',
   opts: {
@@ -22,8 +23,8 @@ const Root = () => {
   return (
     <ApolloProvider client = {client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}>
-        </Route>
+        <Route path="/" component={App} />
+        <Route path="/login" component={LoginForm} />
       </Router>
     </ApolloProvider>
   );
