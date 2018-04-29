f182b0bc70b871c95454a49d0b3173011bff4442
Michael Tombor <tombormt@gmail.com>
2018-04-29 05:12:05 +0000

    add Signup form

---
+++ client/components/SignupForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SignupForm extends Component {
+    render(){
+        return(
+            <div>Sign Up</div>
+            )
+    }
+    
+}
+
+export default SignupForm; 
