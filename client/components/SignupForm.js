ca9c147bd3ddce4b116c0b0fa68e68ea3735bf2a
Michael Tombor <tombormt@gmail.com>
2018-04-29 05:42:50 +0000

    fix bugs for error handling and showing message on AuthForm component

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -7,23 +7,25 @@ import query from '../queries/CurrentUser';
 class SignupForm extends Component {
   constructor(props){
     super(props);
-    this.state = { errors: '' }
+    this.state = { errors: [] }
   }
   
   onSubmit({ email, password }){
   this.props.mutate({
+    //these are the query variables
     variables: { email, password },
     refetchQueries: [{ query }]
   }).catch(res => {
     const errors = res.graphQLErrors.map(error => error.message)
-    this.setState( errors );
+    this.setState({ errors });
   })
   }
   
   render(){
     return(
       <div>
-        <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this) }/>
+        <h3>Sign Up</h3>
+        <AuthForm errors={this.state.errors} onSubmit={ this.onSubmit.bind(this) }/>
       </div>
     )
   }
